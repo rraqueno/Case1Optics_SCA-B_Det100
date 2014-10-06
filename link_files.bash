22c627eeabcfc92615776c99c2f865c1d0750efa
Your Name <you@example.com>
2014-10-06 08:42:17 -0400

    CORRECTED RESULTS BY UPDATING link_files.bash

---
+++ link_files.bash
@@ -0,0 +1,7 @@
+cp -uv MODIS/A2013-309-2110-LAC_SST_geoL8TOA_B11 MODIS.img
+cp -uv MODIS/A2013-309-2110-LAC_SST_geoL8TOA_B11.hdr MODIS.img.hdr
+cp -uv ImageRadLatsLons_Column_100_SCA-B_Det100_case1_309.img L8.img
+cp -uv ImageRadLatsLons_Column_100_SCA-B_Det100_case1_309.hdr L8.hdr
+cp -uv ImageRadLatsLons_Column_100_SCA-B_Det100_case1_309.bll L8.bll
+cp -uv OpticsModelPerDet11_20140527_RITedits_Y_FLIPPEDCSV.csv B11.csv
+
